67f3ba03e0e7e0ce20084333a4434edbf56b27b0
Carlo Quick <98628846+CarloQuick@users.noreply.github.com>
2024-03-31 17:00:28 -0400

    homepage and images

--- algomindf/pages/homepage.jsx
+++ algomindf/pages/homepage.jsx
@@ -1,14 +1,13 @@
 "use client";
 import React, { useState } from "react";
-import Image from "next/image"; 
+import Image from "next/image";
 import "../src/app/globals.css";
 import { useRouter } from "next/navigation";
-import Lesson from "../src/components/Lesson"; 
+import Lesson from "../src/components/Lesson";
 import logo_alligator from "../src/images/logo_alligator.png";
 import sample_q_1 from "../src/images/sample_q_1.png";
 import sample_q_2 from "../src/images/sample_q_2.png";
-import SampleQuestions from "../src/components/SampleQuestions";
-
+//import SampleQuestions from "../src/components/SampleQuestions";
 
 const Homepage = () => {
   // Define state for tabs
@@ -58,46 +57,46 @@ const Homepage = () => {
       {/* Main content */}
       <div className="p-4 flex justify-between items-center">
         <div>
-          <Image src={logo_alligator} width="500" height="500"/>
+          <Image src={logo_alligator} width="500" height="500" />
         </div>
 
         <div className="flex flex-col w-full md:w-3/4 lg:w-1/2 px-4 font-nunito">
           {/* <div style={{ fontFamily: "poppins" }}> */}
-            {activeTab === "home" && (
-              <p>
-                Welcome! Unlock the power of data structures and algorithms with
-                AlgoMind, your essential tool for mastering the core concepts.
-                Build a rock-solid foundation and propel your programming skills
-                to new heights. Let's embark on this learning journey together!
-              </p>
-            )}
-            {activeTab === "about" && (
-              <p>
-                {" "}
-                Drawing from our own experiences with learning data structures
-                and algorithms at the University of Florida, we aim to develop a
-                platform that simplifies these concepts into manageable steps.
-                We've incorporated gamified elements such as levels, experience
-                points, and badges to make learning an enjoyable experience. Our
-                goal is to cultivate a smoother understanding of the subject.
-                <br /> <br /> Happy Learning! <br /> <br /> Yours truly, <br />{" "}
-                The AlgoMind Assembly
-              </p>
-            )}
-            {activeTab === "sample questions" && (
-              <div>
-                <p>Here are some questions that you will see in the lessons.</p>
-                  <div className="flex-col mt-4">
-                    {/* <Image src={sample_q_1} width="275" height="275"/> */}
-                    {/* <Image
+          {activeTab === "home" && (
+            <p>
+              Welcome! Unlock the power of data structures and algorithms with
+              AlgoMind, your essential tool for mastering the core concepts.
+              Build a rock-solid foundation and propel your programming skills
+              to new heights. Let's embark on this learning journey together!
+            </p>
+          )}
+          {activeTab === "about" && (
+            <p>
+              {" "}
+              Drawing from our own experiences with learning data structures and
+              algorithms at the University of Florida, we aim to develop a
+              platform that simplifies these concepts into manageable steps.
+              We've incorporated gamified elements such as levels, experience
+              points, and badges to make learning an enjoyable experience. Our
+              goal is to cultivate a smoother understanding of the subject.
+              <br /> <br /> Happy Learning! <br /> <br /> Yours truly, <br />{" "}
+              The AlgoMind Assembly
+            </p>
+          )}
+          {activeTab === "sample questions" && (
+            <div>
+              <p>Here are some questions that you will see in the lessons.</p>
+              <div className="flex-col mt-4">
+                {/* <Image src={sample_q_1} width="275" height="275"/> */}
+                {/* <Image
                     src={sample_q_2}
                     width="275"
                     height="275"
                     /> */}
-                    {/* <SampleQuestions /> */}
-                  </div>  
+                {/* <SampleQuestions /> */}
               </div>
-            )}
+            </div>
+          )}
           {/* </div> */}
 
           {activeTab === "home" && (
