1831e4980476b885e7cfb7a6e331d3632d0b5510
Carlo Quick <98628846+CarloQuick@users.noreply.github.com>
2024-04-13 19:15:17 -0400

    ds render on buttons

--- algomindf/src/components/LessonBtn.jsx
+++ algomindf/src/components/LessonBtn.jsx
@@ -4,7 +4,13 @@ export default function LessonBtn({ ds }) {
   return (
     <div>
       <div>
-        <Link href={`/lessonmap/${ds}/1`}>Stack 1</Link>
+        <Link href={`/lessonmap/${ds}/1`}>{ds} 1</Link>
+      </div>
+      <div>
+        <Link href={`/lessonmap/${ds}/2`}>{ds} 2</Link>
+      </div>
+      <div>
+        <Link href={`/lessonmap/${ds}/3`}>{ds} 3</Link>
       </div>
     </div>
   );
